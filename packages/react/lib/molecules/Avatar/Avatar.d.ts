89be9ee1d8a48f162456d24b9836d2d0a7fb93aa
Akash Phago <akash.phago@bizcaps.com>
2026-01-01 19:16:49 +1100

    Created Avatar Component on packages/react

---
+++ packages/react/lib/molecules/Avatar/Avatar.d.ts
@@ -0,0 +1,25 @@
+import React from "react";
+export declare const KEY_CODES: {
+    ENTER: string;
+    SPACE: string;
+    DOWN_ARROW: string;
+    ESC: string;
+    UP_ARROW: string;
+};
+export interface SelectOption {
+    label: string;
+    value: string;
+}
+export interface RenderOptionProps {
+    isSelected: boolean;
+    option: SelectOption;
+    getOptionRecommendedProps: (overrideProps?: Object) => Object;
+}
+export interface SelectProps {
+    onOptionSelected?: (option: SelectOption, optionIndex: number) => void;
+    options?: SelectOption[];
+    label?: string;
+    renderOption?: (props: RenderOptionProps) => React.ReactNode;
+}
+declare const Avatar: React.FunctionComponent<SelectProps>;
+export default Avatar;
